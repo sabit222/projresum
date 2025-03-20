496b124147d0e4ff1c1729df4cdca52f78745478
Sabit222 <sr.rakhim@gmail.com>
2025-03-20 13:15:22 +0500

    Классы регистрации и аутентификации пользователей

--- user_module/src/main/java/userservice/auth/AuthenticationService.java
+++ user_module/src/main/java/userservice/auth/AuthenticationService.java
@@ -1,8 +1,13 @@
 package userservice.auth;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import userservice.config.JwtService;
@@ -11,6 +16,8 @@ import userservice.model.TokenType;
 import userservice.model.User;
 import userservice.repository.TokenRepository;
 import userservice.repository.UserRepository;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -41,8 +48,24 @@ public class AuthenticationService {
                 .build();
     }
 
-
-
+public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getEmail(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(authenticationRequest.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .build();
+}
 
     private void saveUserToken(User user, String jwtToken){
         var token = Token.builder()
@@ -53,5 +76,45 @@ public class AuthenticationService {
                 .revoked(false)
                 .build();
         tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(User user){
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException{
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String userEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            return;
+        }
+        refreshToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(refreshToken);
+        if (userEmail != null){
+            var user = this.userRepository.findByEmail(userEmail)
+                    .orElseThrow();
+            if (jwtService.isTokenValid(refreshToken, user)){
+                var accessToken = jwtService.generateToken(user);
+                revokeAllUserTokens(user);
+                saveUserToken(user, accessToken);
+                var authResponse = AuthenticationResponse.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken)
+                        .build();
+                new ObjectMapper().writeValue(response.getOutputStream(),authResponse);
+            }
+        }
     }
 }
---
+++ user_module/src/main/java/userservice/model/UpdateUserRequest.java
@@ -0,0 +1,19 @@
+package userservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserRequest {
+
+
+    private String firstname;
+    private String lastname;
+    private String email;
+}
